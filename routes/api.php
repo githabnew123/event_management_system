c26650764a75fef3c1d61eb390473eb2c629313d
githabnew123 <56070538+githabnew123@users.noreply.github.com>
2023-10-20 16:45:20 +0630

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Event_User_Controller;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\ReviewController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +20,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('event_user_control', Event_User_Controller::class);
+Route::resource('eventControl', EventController::class);
+Route::resource('reviewControl', ReviewController::class);
