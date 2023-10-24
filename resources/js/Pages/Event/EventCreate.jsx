c3a33070e83b00524e67166a2b12937036474bf1
Htoo Aung Lynn <htooaunglynn6@gmail.com>
2023-10-24 09:08:03 +0630

    Event Create Page add

--- resources/js/Pages/Event/EventCreate.jsx
+++ resources/js/Pages/Event/EventCreate.jsx
@@ -32,8 +32,9 @@ export default function Register() {
         // </>
         <GuestLayout>
             {/* <Head title="Register" /> */}
-            <h1>Hello Event Create</h1>
-            {/* <form onSubmit={submit}>
+            <h1 className='text-center'>Event Register</h1>
+            <form onSubmit={submit}>
+                {/* Event Name */}
                 <div>
                     <InputLabel htmlFor="name" value="Name" />
 
@@ -50,71 +51,216 @@ export default function Register() {
 
                     <InputError message={errors.name} className="mt-2" />
                 </div>
+                {/* Event Name */}
 
+
+                {/* Event Type */}
                 <div className="mt-4">
-                    <InputLabel htmlFor="email" value="Email" />
+                    <InputLabel htmlFor="type" value="Type" />
 
                     <TextInput
-                        id="email"
-                        type="email"
-                        name="email"
-                        value={data.email}
+                        id="type"
+                        name="type"
+                        value={data.type}
                         className="mt-1 block w-full"
-                        autoComplete="username"
-                        onChange={(e) => setData('email', e.target.value)}
+                        autoComplete="type"
+                        isFocused={true}
+                        onChange={(e) => setData('type', e.target.value)}
                         required
                     />
 
-                    <InputError message={errors.email} className="mt-2" />
+                    <InputError message={errors.type} className="mt-2" />
                 </div>
+                {/* Event Type */}
 
+
+                
+
+
+                {/* Event Date */}
                 <div className="mt-4">
-                    <InputLabel htmlFor="password" value="Password" />
+                    <InputLabel htmlFor="date" value="Date" />
 
                     <TextInput
-                        id="password"
-                        type="password"
-                        name="password"
-                        value={data.password}
+                        id="date"
+                        name="date"
+                        value={data.date}
                         className="mt-1 block w-full"
-                        autoComplete="new-password"
-                        onChange={(e) => setData('password', e.target.value)}
+                        autoComplete="date"
+                        isFocused={true}
+                        onChange={(e) => setData('date', e.target.value)}
                         required
                     />
 
-                    <InputError message={errors.password} className="mt-2" />
+                    <InputError message={errors.date} className="mt-2" />
                 </div>
+                {/* Event Date */}
 
+                {/* Event Time */}
                 <div className="mt-4">
-                    <InputLabel htmlFor="password_confirmation" value="Confirm Password" />
+                    <InputLabel htmlFor="time" value="Time" />
 
                     <TextInput
-                        id="password_confirmation"
-                        type="password"
-                        name="password_confirmation"
-                        value={data.password_confirmation}
+                        id="time"
+                        name="time"
+                        value={data.time}
                         className="mt-1 block w-full"
-                        autoComplete="new-password"
-                        onChange={(e) => setData('password_confirmation', e.target.value)}
+                        autoComplete="time"
+                        isFocused={true}
+                        onChange={(e) => setData('time', e.target.value)}
                         required
                     />
 
-                    <InputError message={errors.password_confirmation} className="mt-2" />
+                    <InputError message={errors.time} className="mt-2" />
                 </div>
+                {/* Event Time */}
+
+                {/* Event Capacity */}
+                <div className="mt-4">
+                    <InputLabel htmlFor="capacity" value="Capacity" />
+                    
+                    <TextInput
+                        type="number"
+                        id="capacity"
+                        name="capacity"
+                        value={data.capacity}
+                        className="mt-1 block w-full"
+                        autoComplete="capacity"
+                        isFocused={true}
+                        onChange={(e) => setData('capacity', e.target.value)}
+                        required
+                    />
+
+                    <InputError message={errors.capacity} className="mt-2" />
+                </div>
+                {/* Event Capacity */}
+
+
+                {/* Event Avater */}
+                <div className="mt-4">
+                    <InputLabel htmlFor="avater" value="Avater" />
+                    
+                    <TextInput
+                        type="file"
+                        id="avater"
+                        name="avater"
+                        value={data.avater}
+                        className="mt-1 block w-full"
+                        autoComplete="avater"
+                        isFocused={true}
+                        onChange={(e) => setData('avater', e.target.value)}
+                        required
+                    />
+
+                    <InputError message={errors.avater} className="mt-2" />
+                </div>
+                {/* Event Avater */}
+
+
+                {/* Event Description */}
+                <div className="mt-4">
+                    <InputLabel htmlFor="description" value="Description" />
+                    
+                    <TextInput
+                        id="description"
+                        name="description"
+                        value={data.description}
+                        className="mt-1 block w-full"
+                        autoComplete="description"
+                        isFocused={true}
+                        onChange={(e) => setData('description', e.target.value)}
+                        required
+                    />
+
+                    <InputError message={errors.description} className="mt-2" />
+                </div>
+                {/* Event Description */}
+
+
+                {/* Event Price */}
+                <div className="mt-4">
+                    <InputLabel htmlFor="price" value="Price" />
+                    
+                    <TextInput
+                        type="number"
+                        id="price"
+                        name="price"
+                        value={data.price}
+                        className="mt-1 block w-full"
+                        autoComplete="price"
+                        isFocused={true}
+                        onChange={(e) => setData('price', e.target.value)}
+                        required
+                    />
+
+                    <InputError message={errors.price} className="mt-2" />
+                </div>
+                {/* Event Price */}
+
+                {/* Event User Type */}
+                <div className="mt-4">
+                    <InputLabel htmlFor="usertype" value="User Type" />
+                    
+                    <TextInput
+                        id="usertype"
+                        name="usertype"
+                        value={data.usertype}
+                        className="mt-1 block w-full"
+                        autoComplete="usertype"
+                        isFocused={true}
+                        onChange={(e) => setData('usertype', e.target.value)}
+                        required
+                    />
+
+                    <InputError message={errors.usertype} className="mt-2" />
+                </div>
+                {/* Event User Type */}
+
+
+                {/* Event Location */}
+                <div className="mt-4">
+                    <InputLabel htmlFor="location" value="Location" />
+
+                    <textarea
+                        name="location"
+                        id="location"
+                        value={data.location}
+                        className="mt-1 block w-full"
+                        cols="20" rows="10"
+                        isFocused={true}
+                        onChange={(e) => setData('location', e.target.value)}
+                        required
+                    ></textarea>
+
+                    {/* <TextInput
+                        id="location"
+                        name="location"
+                        value={data.location}
+                        className="mt-1 block w-full"
+                        autoComplete="location"
+                        isFocused={true}
+                        onChange={(e) => setData('location', e.target.value)}
+                        required
+                    /> */}
+
+                    <InputError message={errors.location} className="mt-2" />
+                </div>
+                {/* Event Location */}
+                
 
                 <div className="flex items-center justify-end mt-4">
-                    <Link
+                    {/* <Link
                         href={route('login')}
                         className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                     >
                         Already registered?
-                    </Link>
+                    </Link> */}
 
                     <PrimaryButton className="ml-4" disabled={processing}>
-                        Register
+                        Create
                     </PrimaryButton>
                 </div>
-            </form> */}
+            </form>
         </GuestLayout>
     );
 }
